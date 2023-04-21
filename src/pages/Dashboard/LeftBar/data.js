7df9ee0ed56c30c6bbf3583c10aeee23dba6d1b6
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-04-21 23:59:12 +0200

    fixed erros

---
+++ src/pages/Dashboard/LeftBar/data.js
@@ -0,0 +1,20 @@
+export const notificationsData = [
+    {
+        profileImg: "/imgs/img1.png",
+        name: "Andrew Thomas",
+        text: "has ordered Apple smart watch 2500mh battery.",
+        timePassed: "25 seconds ago",
+    },
+    {
+        profileImg: "/imgs/img2.png",
+        name: "Andrew Thomas",
+        text: "has received Samsung gadget for charging battery.",
+        timePassed: "30 minutes ago",
+    },
+    {
+        profileImg: "/imgs/img3.png",
+        name: "Andrew Thomas",
+        text: "has ordered Apple smart watch, samsung gear 2500mh battery.",
+        timePassed: "1 hour ago",
+    },
+];
